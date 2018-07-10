c6c14392d7e7c26e274977409105bebaa4ef4fd4
ChepkiRoman <chepki.roman@gmail.com>
2018-07-10 14:38:34 +0300

    hib sample

---
+++ src/main/java/Main.java
@@ -0,0 +1,75 @@
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+    private static SessionFactory factory;
+    private static ServiceRegistry serviceRegistry;
+
+
+    public static void main(String[] args) {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+            serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+                    configuration.getProperties()).build();
+            factory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+
+        listEmployees();
+        addEmployee("V", "n", 123);
+
+    }
+
+    public static Integer addEmployee(String fname, String lname, int salary) {
+        Session session = factory.openSession();
+        Transaction tx = null;
+        Integer employeeID = null;
+
+        try {
+            tx = session.beginTransaction();
+            Employee employee = new Employee(fname, lname, salary);
+            employeeID = (Integer) session.save(employee);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return employeeID;
+    }
+
+    public static void listEmployees() {
+        Session session = factory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            List employees = session.createQuery("FROM Employee").list();
+            for (Iterator iterator = employees.iterator(); iterator.hasNext(); ) {
+                Employee employee = (Employee) iterator.next();
+                System.out.print("First Name: " + employee.getFirstName());
+                System.out.print("  Last Name: " + employee.getLastName());
+                System.out.println("  Salary: " + employee.getSalary());
+            }
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+}
